159dffd4852de50d34958f851f9a1548c9839dbc
nishanth <nishanthreddy4566@gmail.com>
2023-10-14 12:26:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './components/Table';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Table/>
+      
+    </div>
+  );
+}
+
+export default App;
